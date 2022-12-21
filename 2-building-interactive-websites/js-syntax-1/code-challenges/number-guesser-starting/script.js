db63c610d52fc42908926f58f21d92e810945081
belfordj <belfordj@cwu.edu>
2022-12-21 11:29:17 -0800

    finsihed js syntax 1

--- 2-building-interactive-websites/js-syntax-1/code-challenges/number-guesser-starting/script.js
+++ 2-building-interactive-websites/js-syntax-1/code-challenges/number-guesser-starting/script.js
@@ -12,12 +12,89 @@ const generateTarget = () => Math.floor(Math.random() * 10);
 
 const compareGuesses = (humanGuess, compGuess, target) => {
     const humanCalc = () => {
+        if (isNaN(humanGuess)) {
+            alert("Please enter a number between 0 and 9.");
+        } else {
+            return Math.abs(humanGuess - target);
+        }
+    }
+
+    console.log(humanGuess);
+    console.log(compGuess);
+
+
+    const compCalc = () => {
+            return Math.abs(compGuess - target);
+    }
+
+    const cpuVsHuman = (guess1, guess2) => {
+        console.log(guess1);
+        console.log(guess2);
+
+        if (guess1 === guess2) {
+            return true;
+        } else if (guess1 > guess2) {
+            return false;
+        } else if (guess1 < guess2){
+            return true;
+        }
+    }
+    cpuVsHuman(humanCalc(), compCalc());
+}
+
+
+// The purpose of this function is to be used to correctly increase the winner’s score after each round.
+
+const updateScore = (winner) => {
+
+    console.log(winner);
+    
+    if (winner === 'human') {
+        humanScore++;
+    } else {
+        computerScore++;
+    }
+}
+
+
+//The purpose of this function is to be used to update the round number after each round.
+
+const advanceRound = () => {
+    currentRoundNumber++;
+}
+
+
+
+
+
+
+
+
+
+/* FIRST VERSION 
+
+// Write your code below:
+
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// The purpose of this function is to be called at the start of each new round in order to generate the new secret target number.
+const generateTarget = () => Math.floor(Math.random() * 10);
+
+
+// The purpose of this function is to be called each round to determine which guess is closest to the target number.
+
+const compareGuesses = (humanGuess, compGuess, target) => {
+    const humanCalc = () => {
         if (humanGuess > target) {
             return humanGuess - target;
         } else if (humanGuess < target) {
             return target - humanGuess;
         } else if (humanGuess === target){
             return target;
+        } else if (humanGuess < 0 || humanGuess > 9) {
+            alert("Please enter a number between 0 and 9.");
         }
     }
 
@@ -59,3 +136,5 @@ const updateScore = (winner) => {
 const advanceRound = () => {
     currentRoundNumber++;
 }
+
+*/
