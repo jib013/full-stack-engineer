766b581e5845f99437b8d789eab52870138ed98f
belfordj <belfordj@cwu.edu>
2022-12-20 16:07:47 -0800

    starting new branch

--- 2-building-interactive-websites/js-syntax-1/code-challenges/number-guesser-starting/script.js
+++ 2-building-interactive-websites/js-syntax-1/code-challenges/number-guesser-starting/script.js
@@ -4,3 +4,11 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+const generateTarget = Math.floor(Math.random() * 10);
+
+console.log(generateTarget);
